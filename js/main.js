07ae319017e29f0a8d3892377522c292beaa7d2d
usama-ahmad-coeus <usama-ahmad@coeus-solutions.com>
2023-01-27 14:57:02 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -242,6 +242,59 @@ window.onclick = function(event) {
   }
 }
 
+// Get the modal
+var modal8 = document.getElementById("myModal7");
+var modal9 = document.getElementById("myModal8");
+
+// Get the button that opens the modal
+var btn8 = document.getElementById("myBtn8");
+
+// Get the <span> element that closes the modal
+var span8 = document.getElementsByClassName("close8")[0];
+
+// When the user clicks on the button, open the modal
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+}
+
+// Get the modal
+var modal9 = document.getElementById("myModal9");
+
+// Get the button that opens the modal
+var btn9 = document.getElementById("myBtn9");
+
+// Get the <span> element that closes the modal
+var span9 = document.getElementsByClassName("close9")[0];
+
+// When the user clicks on the button, open the modal
+btn9.onclick = function() {
+  modal9.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal9) {
+    modal9.style.display = "none";
+  }
+}
+
 
 
 
